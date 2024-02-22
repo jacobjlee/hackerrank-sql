f3a1516616029f7684d73bbf0f060a3f9dd2e914
Jacob Juhyung Lee <jacob.juhyung.lee@gmail.com>
2024-02-22 16:26:27 +1100

    Add weather observation station 7 solution

---
+++ basic-select/weather_observation_station_7.sql
@@ -0,0 +1,9 @@
+-- Using Regex
+SELECT DISTINCT city
+FROM station
+WHERE city REGEXP '[aeiou]$';
+
+-- Using RIGHT() function
+SELECT DISTINCT city
+FROM station
+WHERE RIGHT(city, 1) IN ('a', 'e', 'i', 'o', 'u');
